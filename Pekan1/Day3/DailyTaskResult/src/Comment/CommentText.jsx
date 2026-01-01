d244297067f23b5f85bfb3c97b8621e8ee064493
S1DxBeelz <yudzzkece984@gmail.com>
2026-01-02 01:26:03 +0700

    Delete Pekan1 directory

--- Pekan1/Day3/DailyTaskResult/src/Comment/CommentText.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CommentText({ text }) {
-  return <p style={{ marginTop: "8px" }}>{text}</p>;
-}
-
-export default CommentText;
--- Pekan1/Day3/DailyTaskResult/src/Greeting.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Greeting({ name }) {
-  return <h2>Halo, {name}!</h2>;
-}
-
-export default Greeting;
